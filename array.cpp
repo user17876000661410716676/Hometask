bac564d423edb4c83f2bea225cc3e3bde016cb95
user17876000661410716676 <noreastnorwest@gmail.com>
2024-10-03 13:33:33 +0300

    Create first programm

---
+++ array.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+const int ROW = 5;
+const int COL = 8;
+
+int main() {
+	/*srand(time(0));
+	int arr[ROW][COL];
+	for (int i = 0; i < ROW; ++i) {
+		for (int j = 0; j < COL; ++j) {
+			arr[i][j] = rand() % 100;
+		}
+	}
+	for (int i = 0; i < ROW; ++i) {
+		for (int j = 0; j < COL; ++j) {
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}*/
+	cout << "Привет";
+}
